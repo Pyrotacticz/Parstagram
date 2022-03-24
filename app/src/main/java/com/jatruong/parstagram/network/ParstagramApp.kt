638882218ff586d6dd17c568ff2c80101597b7c6
jatruong <jatruong@uw.edu>
2022-03-24 12:04:06 -0700

    Added wiring to post, image, and description for a user to take photos and upload to the db.

--- app/src/main/java/com/jatruong/parstagram/network/ParstagramApp.kt
+++ app/src/main/java/com/jatruong/parstagram/network/ParstagramApp.kt
@@ -1,12 +1,18 @@
-package com.jatruong.parstagram
+package com.jatruong.parstagram.network
 
 import android.app.Application
+import com.jatruong.parstagram.R
+import com.jatruong.parstagram.model.Post
 import com.parse.Parse
+import com.parse.ParseObject
 
 class ParstagramApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        ParseObject.registerSubclass(Post::class.java)
+
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
